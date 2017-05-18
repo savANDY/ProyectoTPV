25f5fa74d511f6a14d6c2793dc489ae4666454e1
savANDY <andicuu@gmail.com>
2017-05-18 22:17:46 +0200

    BBDD actualizada

--- WebContent/tpv.sql
+++ WebContent/tpv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-05-2017 a las 15:31:25
+-- Tiempo de generación: 18-05-2017 a las 22:17:24
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 7.1.1
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `categoria` (
   `id` int(3) NOT NULL,
   `nombre` varchar(20) NOT NULL,
-  `imagen` varchar(50) DEFAULT './img/categoria/predeterminado.png',
+  `imagen` varchar(50) DEFAULT './img/categorias/predeterminado.png',
   `color` varchar(6) DEFAULT 'FF6347'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -38,8 +38,10 @@ CREATE TABLE `categoria` (
 --
 
 INSERT INTO `categoria` (`id`, `nombre`, `imagen`, `color`) VALUES
-(1, 'Desayuno', './img/categorias/desayuno.jpg', 'FF8C00'),
-(3, 'Pinchos', './img/categorias/pinchos.png', '57A639');
+(1, 'Desayuno', './img/categorias/desayuno.png', 'FF8C00'),
+(3, 'Pinchos', './img/categorias/pinchos.png', '57A639'),
+(4, 'Bebidas', './img/categorias/bebidas.png', 'FF6347'),
+(5, 'Menu', './img/categorias/menu.png', 'FF6347');
 
 -- --------------------------------------------------------
 
@@ -51,7 +53,7 @@ CREATE TABLE `producto` (
   `id` int(3) NOT NULL,
   `nombre` varchar(20) NOT NULL,
   `categoria` int(3) NOT NULL,
-  `proveedor` int(3) DEFAULT '0',
+  `proveedor` int(3) NOT NULL DEFAULT '1',
   `precio_compra` double NOT NULL,
   `precio_venta` double NOT NULL,
   `imagen` varchar(50) DEFAULT './img/productos/predeterminado.png',
@@ -63,7 +65,13 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`id`, `nombre`, `categoria`, `proveedor`, `precio_compra`, `precio_venta`, `imagen`, `color`) VALUES
-(2, 'Hamburguesa', 1, 1, 0.7, 1.5, './img/productos/hamburguesa.png', 'FFA500');
+(7, 'Hamburguesa', 3, 1, 0.7, 1.5, './img/productos/hamburguesa.png', 'FFA500'),
+(8, 'Cafe Leche', 1, 1, 0.7, 1.4, './img/productos/cafeleche.png', 'FFA500'),
+(9, 'Calimocho', 4, 1, 1.3, 2.5, './img/productos/predeterminado.png', 'FFA500'),
+(11, 'Bocata Jamon Serrano', 5, 1, 2, 3.75, './img/productos/predeterminado.png', 'FFA500'),
+(12, 'De tortilla', 3, 1, 0.8, 1.3, './img/productos/predeterminado.png', 'FFA500'),
+(13, 'Cafe solo', 1, 1, 0.7, 1.3, './img/productos/predeterminado.png', 'FFA500'),
+(14, 'Bollo mantequilla', 1, 1, 0.72, 1, './img/productos/predeterminado.png', 'FFA500');
 
 -- --------------------------------------------------------
 
@@ -84,6 +92,18 @@ CREATE TABLE `proveedor` (
 
 INSERT INTO `proveedor` (`id`, `nombre`, `direccion`, `otros_datos`) VALUES
 (1, 'No Asignado', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id` int(3) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `fecha_venta` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Índices para tablas volcadas
@@ -110,6 +130,12 @@ ALTER TABLE `proveedor`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id`,`id_producto`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -117,12 +143,12 @@ ALTER TABLE `proveedor`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT de la tabla `proveedor`
 --
